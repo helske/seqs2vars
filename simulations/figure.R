3166f4d907f3471b2630ac624424324c99c6cfc3
helske <jouni.helske@iki.fi>
2021-09-08 14:26:52 +0300

    change order of methods, png figure, generalization of experiment code

--- simulations/figure.R
+++ simulations/figure.R
@@ -13,15 +13,19 @@ rmse <- do.call("rbind", lapply(result_files, function(i) {
     estimation_method = rownames(x),
     mean_rmse = as.numeric(rowMeans(x, na.rm = TRUE)),
     lwr = apply(x, 1, quantile, 0.025, na.rm = TRUE),
-    upr = apply(x, 1, quantile, 0.975, na.rm = TRUE))
+    upr = apply(x, 1, quantile, 0.975, na.rm = TRUE),
+    n_na = rowSums(is.na(x)))
 }))
 
+methods <- c("Hard classification", "Soft classification", "Pseudoclass", "Representativeness")
 
 p <- rmse %>%
   mutate(cluster_quality =
       factor(cluster_quality,
         levels = c("fuzzy", "crisp", "super"),
-        labels = c("Weak", "Moderate", "Strong"), ordered = TRUE)) %>%
+        labels = c("Weak", "Moderate", "Strong"), ordered = TRUE),
+    estimation_method = factor(estimation_method,
+      levels = methods, ordered = TRUE)) %>%
   ggplot(aes(x = cluster_quality, y = mean_rmse,
     shape = estimation_method)) +
   geom_pointrange(aes(ymin = lwr, ymax = upr), position = position_dodge(0.6)) +
@@ -30,8 +34,9 @@ p <- rmse %>%
     "Moderate",
     "Strong"))+
   scale_shape_discrete("Estimation method", labels = c(
-    "Hard classification", "Pseudoclass", "Representativeness", "Soft classification")) +
+    "Hard classification", "Soft classification", "Pseudoclass", "Representativeness")) +
   facet_grid(~variable_type,
     labeller = as_labeller(c("class" = "Response based on classification",
       "repr" = "Response based on representativeness"))) +
   theme(legend.position = "bottom")
+ggsave(p, file = "simulation.png")
--- simulations/simulation_experiment.R
+++ simulations/simulation_experiment.R
@@ -12,20 +12,20 @@ registerDoParallel(cl)
 simulation <- function(
   sim_seq,
   y,
-  n = 1000L, nsim = 1000L, ncores = 16,
+  n = 1000L, nsim = 1000L, k = 4,
   seed = sample(1:.Machine$integer.max,size=1)) {
   
   # run FANNY
-  cluster_with_fanny <- function(d, memb.exp) {
-    out <- suppressWarnings(try(fanny(k = 4, diss = TRUE, x = d, memb.exp = memb.exp), silent = TRUE))
+  cluster_with_fanny <- function(d, memb.exp, k = 4) {
+    out <- suppressWarnings(try(fanny(k = k, diss = TRUE, x = d, memb.exp = memb.exp), silent = TRUE))
     if (!inherits(out, "try-error") &&
         abs(out$coeff["normalized"]) > 1e-06 &&
         out$convergence[2] == 1 &&
-        length(unique(out$clustering)) == 4 &&
-        out$k.crisp == 4 &&
-        qr(out$membership)$rank == 4 &&
-        qr(head(out$membership, nrow(d) / 2))$rank == 4 &&
-        qr(tail(out$membership, nrow(d) / 2))$rank == 4) {
+        length(unique(out$clustering)) == k &&
+        out$k.crisp == k &&
+        qr(out$membership)$rank == k &&
+        qr(head(out$membership, nrow(d) / 2))$rank == k &&
+        qr(tail(out$membership, nrow(d) / 2))$rank == k) {
       return(list(out = out))
     } else "error"
   }
@@ -54,14 +54,14 @@ simulation <- function(
       d <- as.matrix(dist_OMsp)
       
       # PAM
-      cluster_pam <- pam(k = 4, diss = TRUE, x = dist_OMsp)
+      cluster_pam <- pam(k = k, diss = TRUE, x = dist_OMsp)
       
       # representativeness to cluster medoids (based on PAM)
       
       d_to_medoid <- 1 - d[, cluster_pam$id.med] / max(d)
       
       # FANNY, with memb.exp 1.4 (can fail)
-      cluster_fanny <- cluster_with_fanny(d, 1.4)
+      cluster_fanny <- cluster_with_fanny(d, 1.4, k = k)
       
       ## Modelling alternatives ##
       
@@ -72,7 +72,7 @@ simulation <- function(
       cl_max <- factor(cluster_pam$clustering)
       df <- data.frame(y = y_sample, cl_max = cl_max) # create data
       fit <- lm(y ~ ., data = head(df, n/2))
-      if (fit$rank == 4) {
+      if (fit$rank == k) {
         # Predict rest
         rmse["Hard classification"] <- sqrt(mean((tail(df$y, n/2) -
             predict(fit, newdata = tail(df, n/2)))^2))
@@ -81,7 +81,7 @@ simulation <- function(
       # Using representativeness
       df <- data.frame(y = y_sample, x = d_to_medoid)
       fit <- lm(y ~  ., data = head(df, n/2))
-      if (fit$rank == 5) {
+      if (fit$rank == (k+1)) {
         rmse["Representativeness"] <- sqrt(mean((tail(df$y, n/2) -
             predict(fit, newdata = tail(df, n/2)))^2))
       }
@@ -91,7 +91,7 @@ simulation <- function(
         x <- cluster_fanny$out$membership
         df <- data.frame(y = y_sample, x = x)
         fit <- lm(y ~ -1 + .,data = head(df, n/2))
-        if (fit$rank == 4) {
+        if (fit$rank == k) {
           rmse["Soft classification"] <- sqrt(mean((tail(df$y, n/2) -
               predict(fit, newdata = tail(df, n/2)))^2))
         }
@@ -99,10 +99,10 @@ simulation <- function(
         # Averaging predictions 
         # This is simpler than averaging coefficients but gives equal results in the linear model case
         pseudo_preds <- matrix(NA, n/2, 50)
-        df <- data.frame(y = y_sample, x = factor(1:4))
+        df <- data.frame(y = y_sample, x = factor(NA, levels = 1:k))
         for(jj in 1:50) {
           df$x <- factor(apply(x, 1, function(p) sample(1:length(p), size = 1, prob = p)))
-          if(length(unique(df$x)) == 4){
+          if(length(unique(df$x)) == k){
             f <- lm(y ~ -1 + x, data = head(df, n/2))
             pseudo_preds[, jj] <- predict(f, newdata = tail(df, n/2))
           }
@@ -122,17 +122,17 @@ load("fuzzy_data.rds")
 load("crisp_data.rds")
 load("super_crisp_data.rds")
 
-res <- simulation(sim_seq_fuzzy, y_repr_fuzzy, n = 1000, nsim = nsim, seed = 1, ncores = ncores)
+res <- simulation(sim_seq_fuzzy, y_repr_fuzzy, n = 1000, nsim = nsim, seed = 1)
 saveRDS(res, file = "repr_fuzzy.rds")
-res <- simulation(sim_seq_fuzzy, y_class_fuzzy, n = 1000, nsim = nsim, seed = 1, ncores = ncores)
+res <- simulation(sim_seq_fuzzy, y_class_fuzzy, n = 1000, nsim = nsim, seed = 1)
 saveRDS(res, file = "class_fuzzy.rds")
-res <- simulation(sim_seq_crisp, y_repr_crisp, n = 1000, nsim = nsim, seed = 1, ncores = ncores)
+res <- simulation(sim_seq_crisp, y_repr_crisp, n = 1000, nsim = nsim, seed = 1)
 saveRDS(res, file = "repr_crisp.rds")
-res <- simulation(sim_seq_crisp, y_class_crisp, n = 1000, nsim = nsim, seed = 1, ncores = ncores)
+res <- simulation(sim_seq_crisp, y_class_crisp, n = 1000, nsim = nsim, seed = 1)
 saveRDS(res, file = "class_crisp.rds")
-res <- simulation(sim_seq_super_crisp, y_repr_super_crisp, n = 1000, nsim = nsim, seed = 1, ncores = ncores)
+res <- simulation(sim_seq_super_crisp, y_repr_super_crisp, n = 1000, nsim = nsim, seed = 1)
 saveRDS(res, file = "repr_super_crisp.rds")
-res <- simulation(sim_seq_super_crisp, y_class_super_crisp, n = 1000, nsim = nsim, seed = 1, ncores = ncores)
+res <- simulation(sim_seq_super_crisp, y_class_super_crisp, n = 1000, nsim = nsim, seed = 1)
 saveRDS(res, file = "class_super_crisp.rds")
 
 stopCluster(cl)
